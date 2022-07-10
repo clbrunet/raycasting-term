55bf958bf7142af1a3f9601c70ad14996a3f08ba
clbrunet <brunet.clement5@gmail.com>
2022-07-10 09:01:26 +0200

    refactor: add a Player struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::time::Duration;
 
-use nalgebra::{Point2, Vector2};
-use winterm::Window;
 use crossterm::{
     event::{poll, read, Event::Key, KeyCode, KeyEvent},
     style::Color,
     Result,
 };
+use nalgebra::{Point2, Vector2};
+use winterm::Window;
 
 static MAP: [[u8; 8]; 8] = [
     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -19,19 +19,27 @@ static MAP: [[u8; 8]; 8] = [
     [1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-pub struct Raycasting {
+struct Player {
+    position: Point2<f64>,
+    angle: f64,
+}
+
+struct Raycasting {
     window: Window,
     should_stop: bool,
-    player: (Point2<f64>, f64),
+    player: Player,
     horizontal_fov: f64,
 }
 
 impl Raycasting {
     fn new() -> Result<Self> {
         Ok(Raycasting {
-            window: Window::new(58, 72)?,
+            window: Window::new(45, 80)?,
             should_stop: false,
-            player: (Point2::new(4., 4.), 0.),
+            player: Player {
+                position: Point2::new(4., 4.),
+                angle: 0.,
+            },
             horizontal_fov: 60f64.to_radians(),
         })
     }
@@ -42,33 +50,33 @@ impl Raycasting {
         }
 
         if key_event.code == KeyCode::Left {
-            self.player.1 -= 1f64.to_radians();
+            self.player.angle -= 1f64.to_radians();
         }
         if key_event.code == KeyCode::Right {
-            self.player.1 += 1f64.to_radians();
+            self.player.angle += 1f64.to_radians();
         }
 
         let mut movement: Vector2<f64> = Vector2::zeros();
         if key_event.code == KeyCode::Char('w') {
-            movement.x += self.player.1.cos();
-            movement.y += self.player.1.sin();
+            movement.x += self.player.angle.cos();
+            movement.y += self.player.angle.sin();
         }
         if key_event.code == KeyCode::Char('s') {
-            movement.x -= self.player.1.cos();
-            movement.y -= self.player.1.sin();
+            movement.x -= self.player.angle.cos();
+            movement.y -= self.player.angle.sin();
         }
         if movement == Vector2::zeros() {
             return;
         }
         movement.set_magnitude(0.1);
-        self.player.0 += movement;
+        self.player.position += movement;
     }
 
     fn render(&mut self) {
         let angle_increment = self.horizontal_fov / (self.window.width() - 1) as f64;
-        let mut ray_angle = self.player.1 - self.horizontal_fov / 2.;
+        let mut ray_angle = self.player.angle - self.horizontal_fov / 2.;
         for x in 0..self.window.width() {
-            let mut hit = self.player.0;
+            let mut hit = self.player.position;
             hit.x += ray_angle.cos() * 0.05;
             hit.y += ray_angle.sin() * 0.05;
             let mut distance = 0.05;
@@ -80,7 +88,7 @@ impl Raycasting {
                 hit.y += ray_angle.sin() * 0.05;
                 distance += 0.05;
             }
-            distance *= (self.player.1 - ray_angle).cos();
+            distance *= (self.player.angle - ray_angle).cos();
             let height = ((50f64 / distance).round() as u16).min(self.window.height());
             let ceiling_border = ((self.window.height() - height) as f32 / 2.).round() as u16;
             for y in 0..self.window.height() {
